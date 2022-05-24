b0f846da787bf0e03a1858e73078d9dace95d643
Dave Johnston <dave.johnston@harness.io>
2022-05-24 21:23:35 +0100

    Inital commit with feature provider interface
    This is based in the feature provider interface described here:
    https://github.com/open-feature/spec/blob/main/specification/provider/providers.md

---
+++ pkg/openfeature/evaluation_context.go
@@ -0,0 +1,7 @@
+package openfeature
+
+// EvaluationContext
+// https://github.com/open-feature/spec/blob/main/specification/evaluation-context/evaluation-context.md
+type EvaluationContext interface {
+
+}
