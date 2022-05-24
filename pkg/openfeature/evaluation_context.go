d3e3bd4302e6b4d9c83fd2b6b5e4f5ffe5bb6b96
Dave Johnston <dave.johnston@harness.io>
2022-05-24 21:30:56 +0100

    Add NoopProvider to test feature provider interface

--- pkg/openfeature/evaluation_context.go
+++ pkg/openfeature/evaluation_context.go
@@ -3,5 +3,4 @@ package openfeature
 // EvaluationContext
 // https://github.com/open-feature/spec/blob/main/specification/evaluation-context/evaluation-context.md
 type EvaluationContext interface {
-
 }
