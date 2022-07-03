cc2715e4d0613289eb9774bf482f3590c290ce8e
Dave Johnston <dave.johnston@harness.io>
2022-07-03 19:57:51 +0100

    This change adds evaluation calls for the client.
    The Client can call Bool, String, Number, Evaluation functions and get
    corrosponding flag value.
    
    The OpenFeature API gets initalized automatically as a singleton.
    By default it uses the noop provider but this can be overriden by
    calling SetProvider.
    
    Signed-off-by: Dave Johnston <dave.johnston@harness.io>

---
+++ pkg/openfeature/noop_provider.go
@@ -0,0 +1,51 @@
+package openfeature
+
+// NoopProvider implements the FeatureProvider interface and provides functions for evaluating flags
+type NoopProvider struct{}
+
+// Name returns the name of the provider
+func (e NoopProvider) Name() string {
+	return "NoopProvider"
+}
+
+// GetBooleanEvaluation returns a boolean flag.
+func (e NoopProvider) GetBooleanEvaluation(flag string, defaultValue bool, evalCtx EvaluationContext, options ...EvaluationOption) BoolResolutionDetail {
+	return BoolResolutionDetail{
+		Value: defaultValue,
+		ResolutionDetail: ResolutionDetail{
+			Variant: "default-variant",
+			Reason:  DEFAULT,
+		},
+	}
+}
+
+// GetStringEvaluation returns a string flag.
+func (e NoopProvider) GetStringEvaluation(flag string, defaultValue string, evalCtx EvaluationContext, options ...EvaluationOption) StringResolutionDetail {
+	return StringResolutionDetail{
+		Value: defaultValue,
+		ResolutionDetail: ResolutionDetail{
+			Variant: "default-variant",
+			Reason:  DEFAULT,
+		},
+	}
+}
+
+// GetNumberEvaluation returns a number flag.
+func (e NoopProvider) GetNumberEvaluation(flag string, defaultValue float64, evalCtx EvaluationContext, options ...EvaluationOption) NumberResolutionDetail {
+	return NumberResolutionDetail{
+		Value: defaultValue,
+		ResolutionDetail: ResolutionDetail{
+			Variant: "default-variant",
+			Reason:  DEFAULT,
+		},
+	}
+}
+
+// GetObjectEvaluation returns an object flag
+func (e NoopProvider) GetObjectEvaluation(flag string, defaultValue interface{}, evalCtx EvaluationContext, options ...EvaluationOption) ResolutionDetail {
+	return ResolutionDetail{
+		Value:   defaultValue,
+		Variant: "default-variant",
+		Reason:  DEFAULT,
+	}
+}
