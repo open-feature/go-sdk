f0312d81766b1ac9849d4f3c31acf450b452e553
Skye Gill <gill.skye95@gmail.com>
2022-07-08 15:30:45 +0100

    Redefined EvaluationContext as denoted in the spec.
    Signed-off-by: Skye Gill <gill.skye95@gmail.com>

--- pkg/openfeature/evaluation_context.go
+++ pkg/openfeature/evaluation_context.go
@@ -2,5 +2,7 @@ package openfeature
 
 // EvaluationContext
 // https://github.com/open-feature/spec/blob/main/specification/evaluation-context/evaluation-context.md
-type EvaluationContext interface {
+type EvaluationContext struct {
+	TargetingKey string // uniquely identifying the subject (end-user, or client service) of a flag evaluation
+	Attributes map[string]interface{}
 }
