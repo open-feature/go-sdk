7a1014dec555d23e19b5bdb6ae1d5a4c7dd3c9b9
Skye Gill <gill.skye95@gmail.com>
2022-07-20 15:52:13 +0100

    Updated Hook definition and replaced dead link.
    Signed-off-by: Skye Gill <gill.skye95@gmail.com>

--- pkg/openfeature/hooks.go
+++ pkg/openfeature/hooks.go
@@ -1,7 +1,8 @@
 package openfeature
 
-// Hook Hooks are a mechanism whereby application developers can add arbitrary behavior to flag evaluation. They operate similarly to middleware in many web frameworks.
-// https://github.com/open-feature/spec/blob/main/specification/flag-evaluation/hooks.md
+// Hook allows application developers to add arbitrary behavior to the flag evaluation lifecycle.
+// They operate similarly to middleware in many web frameworks.
+// https://github.com/open-feature/spec/blob/main/specification/hooks.md
 type Hook interface {
 	Before(hookContext HookContext, hookHints HookHints) (*EvaluationContext, error)
 	After(hookContext HookContext, flagEvaluationDetails EvaluationDetails, hookHints HookHints) error
