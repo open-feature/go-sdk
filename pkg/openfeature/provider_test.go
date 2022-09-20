5300e5ef58bfe56690b216ba7f4a389f0446368f
Akshit42-hue <patelakshit2025@gmail.com>
2022-09-20 23:10:58 +0530

    context_type
    Signed-off-by: Akshit42-hue <patelakshit2025@gmail.com>

--- pkg/openfeature/provider_test.go
+++ pkg/openfeature/provider_test.go
@@ -41,11 +41,11 @@ func TestRequirement_2_2(t *testing.T) {
 	mockProvider := NewMockFeatureProvider(ctrl)
 
 	type requirements interface {
-		BooleanEvaluation(flag string, defaultValue bool, evalCtx map[string]interface{}) BoolResolutionDetail
-		StringEvaluation(flag string, defaultValue string, evalCtx map[string]interface{}) StringResolutionDetail
-		FloatEvaluation(flag string, defaultValue float64, evalCtx map[string]interface{}) FloatResolutionDetail
-		IntEvaluation(flag string, defaultValue int64, evalCtx map[string]interface{}) IntResolutionDetail
-		ObjectEvaluation(flag string, defaultValue interface{}, evalCtx map[string]interface{}) InterfaceResolutionDetail
+		BooleanEvaluation(flag string, defaultValue bool, evalCtx EvaluationContext) BoolResolutionDetail
+		StringEvaluation(flag string, defaultValue string, evalCtx EvaluationContext) StringResolutionDetail
+		FloatEvaluation(flag string, defaultValue float64, evalCtx EvaluationContext) FloatResolutionDetail
+		IntEvaluation(flag string, defaultValue int64, evalCtx EvaluationContext) IntResolutionDetail
+		ObjectEvaluation(flag string, defaultValue interface{}, evalCtx EvaluationContext) InterfaceResolutionDetail
 	}
 
 	var mockProviderI interface{} = mockProvider
