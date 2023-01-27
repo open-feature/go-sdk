6a5ff5dca6071bafef983c8bff17e956f6399ae4
Skye Gill <gill.skye95@gmail.com>
2023-01-27 11:05:38 +0000

    added more fuzz seeds
    Signed-off-by: Skye Gill <gill.skye95@gmail.com>

--- go.mod
+++ go.mod
@@ -37,7 +37,6 @@ require (
 	github.com/prometheus/procfs v0.8.0 // indirect
 	github.com/rs/cors v1.8.3 // indirect
 	github.com/rs/xid v1.4.0 // indirect
-	github.com/sirupsen/logrus v1.9.0 // indirect
 	github.com/spf13/pflag v1.0.5 // indirect
 	github.com/xeipuuv/gojsonpointer v0.0.0-20190905194746-02993c407bfb // indirect
 	github.com/xeipuuv/gojsonreference v0.0.0-20180127040603-bd5ef7bd5415 // indirect
--- integration/evaluation_fuzz_test.go
+++ integration/evaluation_fuzz_test.go
@@ -29,6 +29,9 @@ func FuzzBooleanEvaluation(f *testing.F) {
 	client := setupFuzzClient(f)
 
 	f.Add("foo", false)
+	f.Add("FoO", true)
+	f.Add("FoO234", false)
+	f.Add("FoO2\b34", true)
 	f.Fuzz(func(t *testing.T, flagKey string, defaultValue bool) {
 		res, err := client.BooleanValueDetails(context.Background(), flagKey, defaultValue, openfeature.EvaluationContext{})
 		if err != nil {
@@ -47,6 +50,9 @@ func FuzzStringEvaluation(f *testing.F) {
 	client := setupFuzzClient(f)
 
 	f.Add("foo", "bar")
+	f.Add("FoO", "BaR")
+	f.Add("FoO234", "Ba1232")
+	f.Add("FoO2\b34", "BaaR\b2312")
 	f.Fuzz(func(t *testing.T, flagKey string, defaultValue string) {
 		res, err := client.StringValueDetails(context.Background(), flagKey, defaultValue, openfeature.EvaluationContext{})
 		if err != nil {
@@ -65,6 +71,9 @@ func FuzzIntEvaluation(f *testing.F) {
 	client := setupFuzzClient(f)
 
 	f.Add("foo", int64(1))
+	f.Add("FoO", int64(99))
+	f.Add("FoO234", int64(100029))
+	f.Add("FoO2\b34", int64(-1))
 	f.Fuzz(func(t *testing.T, flagKey string, defaultValue int64) {
 		res, err := client.IntValueDetails(context.Background(), flagKey, defaultValue, openfeature.EvaluationContext{})
 		if err != nil {
@@ -83,6 +92,9 @@ func FuzzFloatEvaluation(f *testing.F) {
 	client := setupFuzzClient(f)
 
 	f.Add("foo", float64(1))
+	f.Add("FoO", 99.9)
+	f.Add("FoO234", 0.00004)
+	f.Add("FoO2\b34", -1.9203)
 	f.Fuzz(func(t *testing.T, flagKey string, defaultValue float64) {
 		res, err := client.FloatValueDetails(context.Background(), flagKey, defaultValue, openfeature.EvaluationContext{})
 		if err != nil {
@@ -101,6 +113,9 @@ func FuzzObjectEvaluation(f *testing.F) {
 	client := setupFuzzClient(f)
 
 	f.Add("foo", "{}")
+	f.Add("FoO", "true")
+	f.Add("FoO234", "-1.23")
+	f.Add("FoO2\b34", "1")
 	f.Fuzz(func(t *testing.T, flagKey string, defaultValue string) { // interface{} is not supported, using a string
 		res, err := client.ObjectValueDetails(context.Background(), flagKey, defaultValue, openfeature.EvaluationContext{})
 		if err != nil {
