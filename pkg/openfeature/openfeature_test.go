29efef18f6c8b884ef1738fd8de375d203144354
Kavindu Dodanduwa <kavindudodanduwa@gmail.com>
2023-06-09 14:13:12 -0700

    add unit tests
    Signed-off-by: Kavindu Dodanduwa <kavindudodanduwa@gmail.com>

--- pkg/openfeature/openfeature_test.go
+++ pkg/openfeature/openfeature_test.go
@@ -21,12 +21,12 @@ func TestRequirement_1_1_1(t *testing.T) {
 		t.Errorf("error setting up provider %v", err)
 	}
 
-	if api.provider() != mockProvider {
+	if getProvider() != mockProvider {
 		t.Error("func SetProvider hasn't set the provider to the singleton")
 	}
 }
 
-// The `API` MUST provide a function to set the global `provider` singleton,
+// The `API` MUST provide a function to set the default `provider`,
 // which accepts an API-conformant `provider` implementation.
 func TestRequirement_1_1_2(t *testing.T) {
 	defer t.Cleanup(initSingleton)
@@ -46,10 +46,62 @@ func TestRequirement_1_1_2(t *testing.T) {
 	}
 }
 
+// The `API` MUST provide a function to bind a given `provider` to one or more client `name`s.
+// If the client-name already has a bound provider, it is overwritten with the new mapping.
+func TestRequirement_1_1_3(t *testing.T) {
+	defer t.Cleanup(initSingleton)
+
+	ctrl := gomock.NewController(t)
+	providerA := NewMockFeatureProvider(ctrl)
+	providerA.EXPECT().Metadata().Return(Metadata{Name: "providerA"}).AnyTimes()
+
+	providerB := NewMockFeatureProvider(ctrl)
+	providerB.EXPECT().Metadata().Return(Metadata{Name: "providerB"}).AnyTimes()
+
+	err := SetNamedProvider("clientA", providerA)
+	if err != nil {
+		t.Errorf("error setting up provider %v", err)
+	}
+
+	err = SetNamedProvider("clientB", providerB)
+	if err != nil {
+		t.Errorf("error setting up provider %v", err)
+	}
+
+	namedProviders := getNamedProviders()
+
+	// Validate binding
+	if len(namedProviders) != 2 {
+		t.Errorf("expected %d providers, but got %d", 2, len(namedProviders))
+	}
+
+	if namedProviders["clientA"] != providerA {
+		t.Errorf("invalid provider binding")
+	}
+
+	if namedProviders["clientB"] != providerB {
+		t.Errorf("invalid provider binding")
+	}
+
+	// Validate overriding: If the client-name already has a bound provider, it is overwritten with the new mapping.
+	providerB2 := NewMockFeatureProvider(ctrl)
+	providerB2.EXPECT().Metadata().Return(Metadata{Name: "providerB2"}).AnyTimes()
+
+	err = SetNamedProvider("clientB", providerB2)
+	if err != nil {
+		t.Errorf("error setting up provider %v", err)
+	}
+
+	namedProviders = getNamedProviders()
+	if namedProviders["clientB"] != providerB2 {
+		t.Errorf("named provider overriding failed")
+	}
+}
+
 // The `API` MUST provide a function to add `hooks` which accepts one or more API-conformant `hooks`,
 // and appends them to the collection of any previously added hooks. When new hooks are added,
 // previously added hooks are not removed.
-func TestRequirement_1_1_3(t *testing.T) {
+func TestRequirement_1_1_4(t *testing.T) {
 	defer t.Cleanup(initSingleton)
 	ctrl := gomock.NewController(t)
 
@@ -58,13 +110,13 @@ func TestRequirement_1_1_3(t *testing.T) {
 	AddHooks(mockHook)
 	AddHooks(mockHook, mockHook)
 
-	if len(api.getHooks()) != 3 {
+	if len(getHooks()) != 3 {
 		t.Error("func AddHooks didn't append the list of hooks to the existing collection of hooks")
 	}
 }
 
 // The API MUST provide a function for retrieving the metadata field of the configured `provider`.
-func TestRequirement_1_1_4(t *testing.T) {
+func TestRequirement_1_1_5(t *testing.T) {
 	defer t.Cleanup(initSingleton)
 	defaultProvider := NoopProvider{}
 	if ProviderMetadata() != defaultProvider.Metadata() {
@@ -74,13 +126,13 @@ func TestRequirement_1_1_4(t *testing.T) {
 
 // The `API` MUST provide a function for creating a `client` which accepts the following options:
 // - name (optional): A logical string identifier for the client.
-func TestRequirement_1_1_5(t *testing.T) {
+func TestRequirement_1_1_6(t *testing.T) {
 	defer t.Cleanup(initSingleton)
 	NewClient("test-client")
 }
 
 // The client creation function MUST NOT throw, or otherwise abnormally terminate.
-func TestRequirement_1_1_6(t *testing.T) {
+func TestRequirement_1_1_7(t *testing.T) {
 	defer t.Cleanup(initSingleton)
 	type clientCreationFunc func(name string) *Client
 
