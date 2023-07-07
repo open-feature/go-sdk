c9cf05a02b4882ca8266d4c8cfbbb6f5cef2d8cf
Kavindu Dodanduwa <kavindudodanduwa@gmail.com>
2023-07-07 14:12:09 -0700

    basic in-memory provider
    Signed-off-by: Kavindu Dodanduwa <kavindudodanduwa@gmail.com>

---
+++ pkg/openfeature/testing/in_memory_provider.go
@@ -0,0 +1,214 @@
+package testing
+
+import (
+	"context"
+	"fmt"
+	"github.com/open-feature/go-sdk/pkg/openfeature"
+)
+
+const (
+	Enabled  State = "ENABLED"
+	Disabled State = "DISABLED"
+)
+
+type InMemoryProvider struct {
+	flags map[string]InMemoryFlag
+}
+
+func NewInMemoryProvider(from map[string]InMemoryFlag) InMemoryProvider {
+	return InMemoryProvider{
+		flags: from,
+	}
+}
+
+func (i InMemoryProvider) Metadata() openfeature.Metadata {
+	return openfeature.Metadata{
+		Name: "InMemoryProvider",
+	}
+}
+
+func (i InMemoryProvider) BooleanEvaluation(ctx context.Context, flag string, defaultValue bool, evalCtx openfeature.FlattenedContext) openfeature.BoolResolutionDetail {
+	memoryFlag, ok := i.flags[flag]
+	if !ok {
+		return openfeature.BoolResolutionDetail{
+			Value: defaultValue,
+			ProviderResolutionDetail: openfeature.ProviderResolutionDetail{
+				ResolutionError: openfeature.NewFlagNotFoundResolutionError(fmt.Sprintf("flag for key %s not found", flag)),
+				Reason:          openfeature.ErrorReason,
+			},
+		}
+	}
+
+	resolveFlag, detail := memoryFlag.Resolve(evalCtx)
+
+	var result bool
+	res, ok := resolveFlag.(bool)
+	if ok {
+		result = res
+	} else {
+		result = defaultValue
+	}
+
+	return openfeature.BoolResolutionDetail{
+		Value:                    result,
+		ProviderResolutionDetail: detail,
+	}
+}
+
+func (i InMemoryProvider) StringEvaluation(ctx context.Context, flag string, defaultValue string, evalCtx openfeature.FlattenedContext) openfeature.StringResolutionDetail {
+	memoryFlag, ok := i.flags[flag]
+	if !ok {
+		return openfeature.StringResolutionDetail{
+			Value: defaultValue,
+			ProviderResolutionDetail: openfeature.ProviderResolutionDetail{
+				ResolutionError: openfeature.NewFlagNotFoundResolutionError(fmt.Sprintf("flag for key %s not found", flag)),
+				Reason:          openfeature.ErrorReason,
+			},
+		}
+	}
+
+	resolveFlag, detail := memoryFlag.Resolve(evalCtx)
+
+	var result string
+	res, ok := resolveFlag.(string)
+	if ok {
+		result = res
+	} else {
+		result = defaultValue
+	}
+
+	return openfeature.StringResolutionDetail{
+		Value:                    result,
+		ProviderResolutionDetail: detail,
+	}
+}
+
+func (i InMemoryProvider) FloatEvaluation(ctx context.Context, flag string, defaultValue float64, evalCtx openfeature.FlattenedContext) openfeature.FloatResolutionDetail {
+	memoryFlag, ok := i.flags[flag]
+	if !ok {
+		return openfeature.FloatResolutionDetail{
+			Value: defaultValue,
+			ProviderResolutionDetail: openfeature.ProviderResolutionDetail{
+				ResolutionError: openfeature.NewFlagNotFoundResolutionError(fmt.Sprintf("flag for key %s not found", flag)),
+				Reason:          openfeature.ErrorReason,
+			},
+		}
+	}
+
+	resolveFlag, detail := memoryFlag.Resolve(evalCtx)
+
+	var result float64
+	res, ok := resolveFlag.(float64)
+	if ok {
+		result = res
+	} else {
+		result = defaultValue
+	}
+
+	return openfeature.FloatResolutionDetail{
+		Value:                    result,
+		ProviderResolutionDetail: detail,
+	}
+}
+
+func (i InMemoryProvider) IntEvaluation(ctx context.Context, flag string, defaultValue int64, evalCtx openfeature.FlattenedContext) openfeature.IntResolutionDetail {
+	memoryFlag, ok := i.flags[flag]
+	if !ok {
+		return openfeature.IntResolutionDetail{
+			Value: defaultValue,
+			ProviderResolutionDetail: openfeature.ProviderResolutionDetail{
+				ResolutionError: openfeature.NewFlagNotFoundResolutionError(fmt.Sprintf("flag for key %s not found", flag)),
+				Reason:          openfeature.ErrorReason,
+			},
+		}
+	}
+
+	resolveFlag, detail := memoryFlag.Resolve(evalCtx)
+
+	var result int64
+	res, ok := resolveFlag.(int64)
+	if ok {
+		result = res
+	} else {
+		result = defaultValue
+	}
+
+	return openfeature.IntResolutionDetail{
+		Value:                    result,
+		ProviderResolutionDetail: detail,
+	}
+}
+
+func (i InMemoryProvider) ObjectEvaluation(ctx context.Context, flag string, defaultValue interface{}, evalCtx openfeature.FlattenedContext) openfeature.InterfaceResolutionDetail {
+	memoryFlag, ok := i.flags[flag]
+	if !ok {
+		return openfeature.InterfaceResolutionDetail{
+			Value: defaultValue,
+			ProviderResolutionDetail: openfeature.ProviderResolutionDetail{
+				ResolutionError: openfeature.NewFlagNotFoundResolutionError(fmt.Sprintf("flag for key %s not found", flag)),
+				Reason:          openfeature.ErrorReason,
+			},
+		}
+	}
+
+	resolveFlag, detail := memoryFlag.Resolve(evalCtx)
+
+	var result interface{}
+	if resolveFlag != nil {
+		result = resolveFlag
+	} else {
+		result = defaultValue
+	}
+
+	return openfeature.InterfaceResolutionDetail{
+		Value:                    result,
+		ProviderResolutionDetail: detail,
+	}
+}
+
+func (i InMemoryProvider) Hooks() []openfeature.Hook {
+	//TODO implement some hooks
+	return []openfeature.Hook{}
+}
+
+// Type Definitions for InMemoryProvider flag
+
+// State of the feature flag
+type State string
+
+// ContextEvaluator is a callback to perform openfeature.EvaluationContext backed evaluations.
+// This is a callback implemented by the flag definer.
+type ContextEvaluator *func(this InMemoryFlag, evalCtx openfeature.FlattenedContext) (interface{}, openfeature.ProviderResolutionDetail)
+
+// InMemoryFlag is the feature flag representation accepted by InMemoryProvider
+type InMemoryFlag struct {
+	Key              string
+	State            State
+	DefaultVariant   string
+	Variants         map[string]interface{}
+	ContextEvaluator ContextEvaluator
+}
+
+func (flag *InMemoryFlag) Resolve(evalCtx openfeature.FlattenedContext) (
+	interface{}, openfeature.ProviderResolutionDetail) {
+
+	// first resolve from context callback
+	if flag.ContextEvaluator != nil {
+		return (*flag.ContextEvaluator)(*flag, evalCtx)
+	}
+
+	// fallback to evaluation
+
+	// check the state
+	if flag.State == Disabled {
+		return nil, openfeature.ProviderResolutionDetail{
+			ResolutionError: openfeature.NewGeneralResolutionError("flag is disabled"),
+			Reason:          openfeature.DisabledReason,
+		}
+	}
+
+	return flag.Variants[flag.DefaultVariant], openfeature.ProviderResolutionDetail{
+		Reason:  openfeature.StaticReason,
+		Variant: flag.DefaultVariant,
+	}
+}
---
+++ pkg/openfeature/testing/in_memory_provider_test.go
@@ -0,0 +1,167 @@
+package testing
+
+import (
+	"context"
+	"github.com/open-feature/go-sdk/pkg/openfeature"
+	"testing"
+)
+
+func TestInMemoryProvider_boolean(t *testing.T) {
+	memoryProvider := NewInMemoryProvider(map[string]InMemoryFlag{
+		"boolFlag": {
+			Key:            "boolFlag",
+			State:          Enabled,
+			DefaultVariant: "true",
+			Variants: map[string]interface{}{
+				"true":  true,
+				"false": false,
+			},
+			ContextEvaluator: nil,
+		},
+	})
+
+	ctx := context.Background()
+
+	t.Run("test boolean success", func(t *testing.T) {
+		evaluation := memoryProvider.BooleanEvaluation(ctx, "boolFlag", false, nil)
+
+		if evaluation.Value != true {
+			t.Errorf("incorect evaluation, expected %t, got %t", true, evaluation.Value)
+		}
+	})
+}
+
+func TestInMemoryProvider_String(t *testing.T) {
+	memoryProvider := NewInMemoryProvider(map[string]InMemoryFlag{
+		"stringFlag": {
+			Key:            "stringFlag",
+			State:          Enabled,
+			DefaultVariant: "stringOne",
+			Variants: map[string]interface{}{
+				"stringOne": "hello",
+				"stringTwo": "GoodBye",
+			},
+			ContextEvaluator: nil,
+		},
+	})
+
+	ctx := context.Background()
+
+	t.Run("test string success", func(t *testing.T) {
+		evaluation := memoryProvider.StringEvaluation(ctx, "stringFlag", "none", nil)
+
+		if evaluation.Value != "hello" {
+			t.Errorf("incorect evaluation, expected %s, got %s", "hello", evaluation.Value)
+		}
+	})
+}
+
+func TestInMemoryProvider_Float(t *testing.T) {
+	memoryProvider := NewInMemoryProvider(map[string]InMemoryFlag{
+		"floatFlag": {
+			Key:            "floatFlag",
+			State:          Enabled,
+			DefaultVariant: "fOne",
+			Variants: map[string]interface{}{
+				"fOne": 1.1,
+				"fTwo": 2.2,
+			},
+			ContextEvaluator: nil,
+		},
+	})
+
+	ctx := context.Background()
+
+	t.Run("test float success", func(t *testing.T) {
+		evaluation := memoryProvider.FloatEvaluation(ctx, "fOne", 1.0, nil)
+
+		if evaluation.Value != 1.0 {
+			t.Errorf("incorect evaluation, expected %f, got %f", 1.0, evaluation.Value)
+		}
+	})
+}
+
+func TestInMemoryProvider_Int(t *testing.T) {
+	memoryProvider := NewInMemoryProvider(map[string]InMemoryFlag{
+		"intFlag": {
+			Key:            "intFlag",
+			State:          Enabled,
+			DefaultVariant: "one",
+			Variants: map[string]interface{}{
+				"one": 1,
+				"two": 2,
+			},
+			ContextEvaluator: nil,
+		},
+	})
+
+	ctx := context.Background()
+
+	t.Run("test integer success", func(t *testing.T) {
+		evaluation := memoryProvider.IntEvaluation(ctx, "IntFlag", 1, nil)
+
+		if evaluation.Value != 1 {
+			t.Errorf("incorect evaluation, expected %d, got %d", 1, evaluation.Value)
+		}
+	})
+}
+
+func TestInMemoryProvider_Object(t *testing.T) {
+	memoryProvider := NewInMemoryProvider(map[string]InMemoryFlag{
+		"objectFlag": {
+			Key:            "objectFlag",
+			State:          Enabled,
+			DefaultVariant: "A",
+			Variants: map[string]interface{}{
+				"A": "SomeResult",
+				"B": "OtherResult",
+			},
+		},
+	})
+
+	ctx := context.Background()
+
+	t.Run("test object success", func(t *testing.T) {
+		evaluation := memoryProvider.ObjectEvaluation(ctx, "objectFlag", "unknown", nil)
+
+		if evaluation.Value != "SomeResult" {
+			t.Errorf("incorect evaluation, expected %v, got %v", "SomeResult", evaluation.Value)
+		}
+	})
+}
+
+func TestInMemoryProvider_WithContext(t *testing.T) {
+	var variantKey = "VariantSelector"
+
+	// simple context handling - variant is selected from key and returned
+	var evaluator = func(callerFlag InMemoryFlag, evalCtx openfeature.FlattenedContext) (interface{}, openfeature.ProviderResolutionDetail) {
+		s := evalCtx[variantKey]
+		return callerFlag.Variants[s.(string)], openfeature.ProviderResolutionDetail{}
+	}
+
+	memoryProvider := NewInMemoryProvider(map[string]InMemoryFlag{
+		"contextFlag": {
+			Key:            "contextFlag",
+			State:          Enabled,
+			DefaultVariant: "true",
+			Variants: map[string]interface{}{
+				"true":  true,
+				"false": false,
+			},
+			ContextEvaluator: &evaluator,
+		},
+	})
+
+	ctx := context.Background()
+
+	t.Run("test with context", func(t *testing.T) {
+
+		evaluation := memoryProvider.BooleanEvaluation(ctx, "contextFlag", true, map[string]interface{}{
+			variantKey: "false",
+		})
+
+		if evaluation.Value != false {
+			t.Errorf("incorect evaluation, expected %v, got %v", false, evaluation.Value)
+		}
+	})
+}
