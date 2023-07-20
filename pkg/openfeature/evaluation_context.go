b0c4fd07000be482746a9b7792466bdf0fd9ea2d
Diego Marangoni <diegomarangoni@me.com>
2023-07-20 14:46:48 +0200

    chore: update broken comment link (#202)
    Signed-off-by: Diego Marangoni <diegomarangoni@me.com>

--- pkg/openfeature/evaluation_context.go
+++ pkg/openfeature/evaluation_context.go
@@ -3,7 +3,7 @@ package openfeature
 // EvaluationContext provides ambient information for the purposes of flag evaluation
 // The use of the constructor, NewEvaluationContext, is enforced to set EvaluationContext's fields in order
 // to enforce immutability.
-// https://github.com/open-feature/spec/blob/main/specification/evaluation-context/evaluation-context.md
+// https://openfeature.dev/specification/sections/evaluation-context
 type EvaluationContext struct {
 	targetingKey string // uniquely identifying the subject (end-user, or client service) of a flag evaluation
 	attributes   map[string]interface{}
