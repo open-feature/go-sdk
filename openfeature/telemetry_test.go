4e62d2b2c7a6bc8dbb97fd424e8864df9e1873f3
bbland1 <104288486+bbland1@users.noreply.github.com>
2025-02-18 23:42:44 -0500

    feat:a base idea of telemetry util after looking at js version
    Signed-off-by: bbland1 <104288486+bbland1@users.noreply.github.com>

---
+++ openfeature/telemetry_test.go
@@ -0,0 +1,19 @@
+package openfeature
+
+import "testing"
+
+func TestCreateEvaluationEvent_1_3_1(t *testing.T){
+
+}
+func TestCreateEvaluationEvent_WithVariant(t *testing.T){
+
+}
+func TestCreateEvaluationEvent_WithFlagMetaData(t *testing.T){
+
+}
+func TestCreateEvaluationEvent_WithErrors(t *testing.T){
+
+}
+func TestCreateEvaluationEvent_withUnknownReason(t *testing.T){
+
+}
