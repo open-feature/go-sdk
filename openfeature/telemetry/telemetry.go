9bf0dc9dd20088053a23bf7ab5267b6437472526
Sahid Velji <sahidvelji@gmail.com>
2025-05-23 21:17:21 -0400

    feat: update telemetry package to conform to latest spec
    Signed-off-by: Sahid Velji <sahidvelji@gmail.com>

--- openfeature/telemetry/telemetry.go
+++ openfeature/telemetry/telemetry.go
@@ -24,86 +24,82 @@ type EvaluationEvent struct {
 	Body map[string]any
 }
 
+// The OpenTelemetry compliant event attributes for flag evaluation.
 const (
-	// The OpenTelemetry compliant event attributes for flag evaluation.
-	// Specification: https://opentelemetry.io/docs/specs/semconv/feature-flags/feature-flags-logs/
+	FlagKey          string = "feature_flag.key"
+	ErrorTypeKey     string = "error.type"
+	ResultValueKey   string = "feature_flag.result.value"
+	ResultVariantKey string = "feature_flag.result.variant"
+	ErrorMessageKey  string = "error.message"
+	ContextIDKey     string = "feature_flag.context.id"
+	ProviderKey      string = "feature_flag.provider.name"
+	ReasonKey        string = "feature_flag.result.reason"
+	FlagSetIDKey     string = "feature_flag.set.id"
+	VersionKey       string = "feature_flag.version"
+)
 
-	TelemetryKey       string = "feature_flag.key"
-	TelemetryErrorCode string = "error.type"
-	TelemetryVariant   string = "feature_flag.variant"
-	TelemetryContextID string = "feature_flag.context.id"
-	TelemetryErrorMsg  string = "feature_flag.evaluation.error.message"
-	TelemetryReason    string = "feature_flag.evaluation.reason"
-	TelemetryProvider  string = "feature_flag.provider_name"
-	TelemetryFlagSetID string = "feature_flag.set.id"
-	TelemetryVersion   string = "feature_flag.version"
+// FlagEvaluationKey is the name of the feature flag evaluation event.
+const FlagEvaluationKey string = "feature_flag.evaluation"
 
-	// Well-known flag metadata attributes for telemetry events.
-	// Specification: https://openfeature.dev/specification/appendix-d#flag-metadata
-	TelemetryFlagMetaContextId string = "contextId"
-	TelemetryFlagMetaFlagSetId string = "flagSetId"
-	TelemetryFlagMetaVersion   string = "version"
-
-	// OpenTelemetry event body.
-	// Specification: https://opentelemetry.io/docs/specs/semconv/feature-flags/feature-flags-logs/
-	TelemetryBody string = "value"
-
-	FlagEvaluationEventName string = "feature_flag.evaluation"
+const (
+	flagMetaContextIDKey string = "contextId"
+	flagMetaFlagSetIDKey string = "flagSetId"
+	flagMetaVersionKey   string = "version"
 )
 
 // CreateEvaluationEvent creates an [EvaluationEvent].
-// It is intended to be used in the `Finally` stage of an [openfeature.Hook].
+// It is intended to be used in the `Finally` stage of a [openfeature.Hook].
 func CreateEvaluationEvent(hookContext openfeature.HookContext, details openfeature.InterfaceEvaluationDetails) EvaluationEvent {
 	attributes := map[string]any{
-		TelemetryKey:      hookContext.FlagKey(),
-		TelemetryProvider: hookContext.ProviderMetadata().Name,
+		FlagKey:     hookContext.FlagKey(),
+		ProviderKey: hookContext.ProviderMetadata().Name,
 	}
 
+	attributes[ReasonKey] = strings.ToLower(string(openfeature.UnknownReason))
 	if details.Reason != "" {
-		attributes[TelemetryReason] = strings.ToLower(string(details.Reason))
-	} else {
-		attributes[TelemetryReason] = strings.ToLower(string(openfeature.UnknownReason))
+		attributes[ReasonKey] = strings.ToLower(string(details.Reason))
 	}
 
-	body := map[string]any{}
+	body := make(map[string]any)
 
 	if details.Variant != "" {
-		attributes[TelemetryVariant] = details.Variant
+		attributes[ResultVariantKey] = details.Variant
 	} else {
-		body[TelemetryBody] = details.Value
+		body[ResultValueKey] = details.Value
 	}
 
-	contextID, exists := details.FlagMetadata[TelemetryFlagMetaContextId]
-	if !exists {
-		contextID = hookContext.EvaluationContext().TargetingKey()
+	attributes[ContextIDKey] = hookContext.EvaluationContext().TargetingKey()
+	if contextID, ok := details.FlagMetadata[flagMetaContextIDKey]; ok {
+		attributes[ContextIDKey] = contextID
 	}
 
-	attributes[TelemetryContextID] = contextID
-
-	setID, exists := details.FlagMetadata[TelemetryFlagMetaFlagSetId]
-	if exists {
-		attributes[TelemetryFlagSetID] = setID
+	if setID, ok := details.FlagMetadata[flagMetaFlagSetIDKey]; ok {
+		attributes[FlagSetIDKey] = setID
 	}
 
-	version, exists := details.FlagMetadata[TelemetryFlagMetaVersion]
-	if exists {
-		attributes[TelemetryVersion] = version
+	if version, ok := details.FlagMetadata[flagMetaVersionKey]; ok {
+		attributes[VersionKey] = version
 	}
 
-	if details.Reason == openfeature.ErrorReason {
-		if details.ErrorCode != "" {
-			attributes[TelemetryErrorCode] = details.ErrorCode
-		} else {
-			attributes[TelemetryErrorCode] = openfeature.GeneralCode
+	if details.Reason != openfeature.ErrorReason {
+		return EvaluationEvent{
+			Name:       FlagEvaluationKey,
+			Attributes: attributes,
+			Body:       body,
 		}
+	}
 
-		if details.ErrorMessage != "" {
-			attributes[TelemetryErrorMsg] = details.ErrorMessage
-		}
+	attributes[ErrorTypeKey] = strings.ToLower(string(openfeature.GeneralCode))
+	if details.ErrorCode != "" {
+		attributes[ErrorTypeKey] = strings.ToLower(string(details.ErrorCode))
+	}
+
+	if details.ErrorMessage != "" {
+		attributes[ErrorMessageKey] = details.ErrorMessage
 	}
 
 	return EvaluationEvent{
-		Name:       FlagEvaluationEventName,
+		Name:       FlagEvaluationKey,
 		Attributes: attributes,
 		Body:       body,
 	}
